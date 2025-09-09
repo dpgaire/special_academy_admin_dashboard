4bc51052b2a65ec1f3b4d0479c76c87116ced553
dpgaire <gairhedurga13@gmail.com>
2025-09-09 19:54:31 +0545

    update the url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const API_BASE_URL = 'https://special-academy-server.vercel.app/api';
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://special-academy-server.vercel.app/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
 
 
 // Create axios instance
